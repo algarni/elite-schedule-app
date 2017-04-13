8d8f49ef2e3232284be4cf9eb5b864d53c343d0f
M. Algarni <m.t.algarni@gmail.com>
2017-04-13 16:09:12 +0300

    Work 13-04-2017

--- src/pages/standings/standings.page.ts
+++ src/pages/standings/standings.page.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import * as _ from 'lodash';
+
+import { EliteApi } from "../../shared/elite-api.service";
 
 @Component({
   selector: 'page-standings',
   templateUrl: 'standings.page.html'
 })
 export class StandingsPage {
+  allStandings: any[];
   standings: any[];
   team: any = {};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+    private navCtrl: NavController,
+    private navParams: NavParams,
+    private eliteApi: EliteApi
+  ) { }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad StandingsPage');
+    this.team = this.navParams.data;
+    let tourneyData = this.eliteApi.getCurrentTourney();
+    this.standings = tourneyData.standings;
+
+    this.allStandings =
+      _.chain(this.standings)
+      .groupBy('division')
+      .toPairs()
+      .map(item => _.zipObject(['divisionName', 'divisionStandings'], item))
+      .value();
+
+      console.log('standings', this.standings);
+      console.log('division standings', this.allStandings);
   }
 
 }
